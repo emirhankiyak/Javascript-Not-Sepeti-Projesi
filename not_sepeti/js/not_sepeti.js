58679fbb860805e60760ac3617a4c3240231a8b7
emirhankiyak <62992416+emirhankiyak@users.noreply.github.com>
2022-09-23 20:26:31 +0300

    Güncelleme
    Tasarım iyileştirmeleri ve js kodları eklendi

--- not_sepeti/js/not_sepeti.js
+++ not_sepeti/js/not_sepeti.js
@@ -1,1 +1,50 @@
-console.log('selam');
+let noteAddButton = document.querySelector('.btn-gorev-ekle');
+let noteDeleteButton = document.querySelector('.gorev-btn .gorev-btn-sil');
+let noteCount = 0;
+noteAddButton.addEventListener('click',addNote);
+noteDeleteButton.addEventListener('click',deleteNote);
+
+function addNote(e){
+    let textInput = document.querySelector('.input-gorev').value;
+    let gorevListesi = document.querySelector('.gorev-listesi')
+    if(!(textInput === "")) {
+        let newNoteElement = document.createElement('div');
+        newNoteElement.className= 'gorev-item';
+
+        let newLi = document.createElement('li');
+        newLi.className='gorev-tanim';
+
+        let newOkButton = document.createElement('button');
+        newOkButton.className = 'gorev-btn gorev-btn-tamamlandi';
+
+        let newDeleteButton = document.createElement('button');
+        newDeleteButton.className = 'gorev-btn gorev-btn-sil';
+
+        let newOkI = document.createElement('i');
+        newOkI.className = 'far fa-check-square';
+
+        let newDeleteI = document.createElement('i');
+        newDeleteI.className = 'far fa-trash-alt';
+
+        newNoteElement.appendChild(newLi);
+        newNoteElement.appendChild(newOkButton);
+        newNoteElement.appendChild(newDeleteButton);
+        newOkButton.appendChild(newOkI);
+        newDeleteButton.appendChild(newDeleteI);
+        newLi.textContent = textInput;
+        gorevListesi.appendChild(newNoteElement);
+        document.querySelector('.input-gorev').value = "";
+        noteCount++;
+    }else alert("Not boş olamaz");
+}
+
+function deleteNote(e){
+    const tiklanilanEleman = e.target;
+    if(tiklanilanEleman.classList.contains('gorev-btn-tamamlandi')){
+        tiklanilanEleman.parentElement.classList.toggle('gorev-tamamlandi');
+        console.log('butona tıklandı');
+    }
+    if(tiklanilanEleman.classList.contains('gorev-btn-sil')){
+        
+    }
+}
